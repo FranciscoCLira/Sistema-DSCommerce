24c7468142ce7c85e1c35a8142dc4a9ee93011c1
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2025-07-21 03:16:35 -0300

    Revert "DESAFIO Modelo de domínio e ORM - concluído"
    This reverts commit 84d3ef838d7402e2d9979d05cd89d40ad72d2dfa.

--- src/main/java/com/devsuperior/desafio002/repositories/ParticipanteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devsuperior.desafio002.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devsuperior.desafio002.entities.Participante;
-
-public interface ParticipanteRepository extends JpaRepository<Participante, Integer> {
-}
